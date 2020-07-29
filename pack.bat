30d7aec88d38c3a7b9efbefc1c6c07e9a08221ee
Shaojun Li <i@shaojun.li>
2020-07-29 11:37:14 +0800

    Fix scripts again.

--- pack.bat
+++ pack.bat
@@ -1,16 +1,17 @@
-for /f %%i in ('grep -oP "(?<=<Version>).*(?=</Version>)" WindowDebugger/WindowDebugger.csproj') do set Version= %%i
+for /f %%i in ('grep -oP "(?<=<Version>).*(?=</Version>)" WindowDebugger/WindowDebugger.csproj') do set Version=%%i
 
 if "%Version%"=="" exit -1
 
-for /f %%i in ('git tag ^| grep %Version%') do set IsExist= %%i
+for /f %%i in ('git tag ^| grep %Version%') do set IsExist=%%i
 
-if "%IsExist%"=="" (
+if "%IsExist%"=="" goto DoRelease
+
+::exist
+echo No Need To Release
+exit 0
+
+:DoRelease
 ::not exist
 curl --ssl-no-revoke -H "JOB-TOKEN: %CI_JOB_TOKEN%" -H "Content-Type: application/json" -d "{ """name""": """%Version%""", """tag_name""": """%Version%""", """ref""": """%CI_BUILD_REF%"""}" "%CI_API_V4_URL%/projects/%CI_PROJECT_ID%/releases"
 7z a WindowDebugger.netcoreapp3.1.7z .\WindowDebugger\bin\Release\netcoreapp3.1\*
 ::TODO Upload to somewhere
-
-) else (
-::exist
-echo No Need To Release
-)

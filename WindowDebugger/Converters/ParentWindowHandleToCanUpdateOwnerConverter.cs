25b5d681c5bd5ea62bfe6c6b10cdfbbb00f66088
Shaojun Li <i@shaojun.li>
2021-11-16 21:39:04 +0800

    Fix UpdateOwnerWindow IsEnabled.

---
+++ WindowDebugger/Converters/ParentWindowHandleToCanUpdateOwnerConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using static Lsj.Util.Win32.User32;
+
+namespace WindowDebugger.Converters
+{
+    public class ParentWindowHandleToCanUpdateOwnerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is IntPtr parentWindowHandle && parentWindowHandle == GetDesktopWindow();
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}

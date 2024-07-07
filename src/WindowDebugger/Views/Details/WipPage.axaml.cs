2dabcc6b33e47e6998838e11a1433b75e17df1be
walterlv <xblero@gmail.com>
2024-07-07 19:20:55 +0800

    common features.

---
+++ src/WindowDebugger/Views/Details/WipPage.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WindowDebugger.Views.Details;
+
+public partial class WipPage : UserControl
+{
+    public WipPage()
+    {
+        InitializeComponent();
+    }
+}
+
--- src/WindowDebugger/Views/MainView.axaml.cs
+++ src/WindowDebugger/Views/MainView.axaml.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia.Controls;
+﻿using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 using WindowDebugger.Localizations;
 using WindowDebugger.Services.NativeWindows.Windows;
+using WindowDebugger.Views.Details;
 using WindowDebugger.Views.Details.Windows;
 
 namespace WindowDebugger.Views;
@@ -16,23 +19,40 @@ public partial class MainView : UserControl
         Loaded += OnLoaded;
     }
 
+    private void TopMostToggleButton_IsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        if (TopLevel.GetTopLevel(this) is Window window)
+        {
+            window.Topmost = ((ToggleButton)sender!).IsChecked is true;
+        }
+    }
+
+    private void UacButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (Environment.ProcessPath is { } path && File.Exists(path))
+        {
+            Process.Start(new ProcessStartInfo(path)
+            {
+                UseShellExecute = true,
+                Verb = "runas",
+            });
+            if (TopLevel.GetTopLevel(this) is Window window)
+            {
+                window.Close();
+            }
+        }
+    }
+
     private async void OnLoaded(object? sender, RoutedEventArgs e)
     {
         var vm = new MainViewModel();
         DataContext = vm;
+        await ReloadAllAsync();
+    }
 
-        vm.ReloadWindows();
-
-        var selfId = Environment.ProcessId;
-        var defaultSelection = vm.NativeWindows.FirstOrDefault(x => x.ProcessId == selfId);
-        if (defaultSelection is not null)
-        {
-            await Task.Delay(1);
-            var index = vm.NativeWindows.IndexOf(defaultSelection);
-            WindowListBox.ScrollIntoView(vm.NativeWindows[^1]);
-            WindowListBox.ScrollIntoView(vm.NativeWindows[Math.Max(0, index - 1)]);
-            WindowListBox.SelectedItem = defaultSelection;
-        }
+    private async void ReloadAllButton_Click(object? sender, RoutedEventArgs e)
+    {
+        await ReloadAllAsync();
     }
 
     private void ReloadButton_Click(object? sender, RoutedEventArgs e)
@@ -42,6 +62,40 @@ public partial class MainView : UserControl
         WindowListBox.SelectedItem = oldSelection;
     }
 
+    private async Task ReloadAllAsync()
+    {
+        var vm = (MainViewModel)DataContext!;
+        var oldSelection = WindowListBox.SelectedItem as WindowsNativeWindowModel;
+
+        vm.ReloadWindows();
+
+        var selfId = Environment.ProcessId;
+        var newSelection = vm.NativeWindows.FirstOrDefault(x => x.Id == oldSelection?.Id);
+        var defaultSelection = newSelection ?? vm.NativeWindows.FirstOrDefault(x => x.ProcessId == selfId);
+        if (defaultSelection is not null)
+        {
+            if (newSelection is null)
+            {
+                // 初次选择，或者此前已取消选择。
+                var index = vm.NativeWindows.IndexOf(defaultSelection);
+                await Task.Delay(0);
+                WindowListBox.ScrollIntoView(vm.NativeWindows[^1]);
+                await Task.Delay(0);
+                WindowListBox.ScrollIntoView(vm.NativeWindows[Math.Max(0, index - 1)]);
+                await Task.Delay(0);
+                WindowListBox.SelectedItem = defaultSelection;
+            }
+            else
+            {
+                // 曾经已选择，刷新后重新选择。
+                await Task.Delay(0);
+                var index = vm.NativeWindows.IndexOf(defaultSelection);
+                WindowListBox.ScrollIntoView(vm.NativeWindows[Math.Max(0, index)]);
+                WindowListBox.SelectedItem = defaultSelection;
+            }
+        }
+    }
+
     private void InitializePlatformPages()
     {
         if (OperatingSystem.IsLinux())
@@ -49,17 +103,17 @@ public partial class MainView : UserControl
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Linux.Info.Title,
-                Content = new WindowsInfoPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Linux.Properties.Title,
-                Content = new WindowsInfoPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Linux.Operations.Title,
-                Content = new WindowsInfoPage(),
+                Content = new WipPage(),
             });
         }
         else if (OperatingSystem.IsWindows())
@@ -72,32 +126,32 @@ public partial class MainView : UserControl
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Windows.Styles.Title,
-                Content = new WindowsStylesPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Windows.StyleExes.Title,
-                Content = new WindowStyleExesPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Windows.ClassStyles.Title,
-                Content = new WindowClassStylesPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Windows.Operations.Title,
-                Content = new WindowOperationsPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Windows.Dwm.Title,
-                Content = new WindowDwmPage(),
+                Content = new WipPage(),
             });
             WindowDetailTabControl.Items.Add(new TabItem
             {
                 Header = Lang.Current.App.UI.WindowDetail.Pages.Windows.Others.Title,
-                Content = new WindowOthersPage(),
+                Content = new WipPage(),
             });
         }
     }

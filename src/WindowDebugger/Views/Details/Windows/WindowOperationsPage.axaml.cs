68509e00b9c400820dbf845e57784f5627fc0b88
walterlv <xblero@gmail.com>
2024-07-08 12:34:18 +0800

    operations page

--- src/WindowDebugger/Views/Details/Windows/WindowOperationsPage.axaml.cs
+++ src/WindowDebugger/Views/Details/Windows/WindowOperationsPage.axaml.cs
@@ -1,6 +1,6 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using WindowDebugger.Services.NativeWindows.Windows;
 
 namespace WindowDebugger.Views.Details.Windows;
 
@@ -10,5 +10,51 @@ public partial class WindowOperationsPage : UserControl
     {
         InitializeComponent();
     }
-}
 
+    private WindowsNativeWindowModel? NativeWindow => DataContext as WindowsNativeWindowModel;
+
+    private void SetForegroundButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.SetForeground();
+    }
+
+    private void CloseWindowButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.CloseWindow();
+    }
+
+    private void RedrawWindowButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.RedrawWindow();
+    }
+
+    private void FlashWindowButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.FlashWindow();
+    }
+
+    private void KillProcessButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.KillProcess();
+    }
+
+    private void ForceKillProcessButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.ForceKillProcess();
+    }
+
+    private void SuspendThreadButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.SuspendThread();
+    }
+
+    private void ResumeThreadButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.ResumeThread();
+    }
+
+    private void KillThreadButton_Click(object? sender, RoutedEventArgs e)
+    {
+        NativeWindow?.KillThread();
+    }
+}

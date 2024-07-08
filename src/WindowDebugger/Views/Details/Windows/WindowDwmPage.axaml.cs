831e9ef5b6996eb4559cd5963dbdd776c4bcc32d
walterlv <xblero@gmail.com>
2024-07-08 09:27:01 +0800

    make header clearer

--- src/WindowDebugger/Views/Details/Windows/WindowDwmPage.axaml.cs
+++ src/WindowDebugger/Views/Details/Windows/WindowDwmPage.axaml.cs
@@ -1,14 +1,32 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Lsj.Util.Win32;
 
 namespace WindowDebugger.Views.Details.Windows;
 
 public partial class WindowDwmPage : UserControl
 {
+    private bool _dwmIsCompositionEnabled;
+
     public WindowDwmPage()
     {
         InitializeComponent();
+
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    public static readonly DirectProperty<WindowDwmPage, bool> DwmIsCompositionEnabledProperty = AvaloniaProperty.RegisterDirect<WindowDwmPage, bool>(
+        "DwmIsCompositionEnabled", o => o.DwmIsCompositionEnabled, (o, v) => o.DwmIsCompositionEnabled = v);
+
+    public bool DwmIsCompositionEnabled
+    {
+        get => _dwmIsCompositionEnabled;
+        set => SetAndRaise(DwmIsCompositionEnabledProperty, ref _dwmIsCompositionEnabled, value);
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        Dwmapi.DwmIsCompositionEnabled(out var isEnabled);
+        DwmIsCompositionEnabled = isEnabled;
     }
 }
-

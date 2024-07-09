7ebe4cc7eff5cdf110ff3c2ee1f006100c60400a
walterlv <xblero@gmail.com>
2024-07-09 10:58:29 +0800

    Add linux support (first commit)

---
+++ src/WindowDebugger/Services/NativeWindows/Linux/LinuxNativeWindowModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WindowDebugger.Services.NativeWindows.Linux;
+
+public record LinuxNativeWindowModel(nint Id) : NativeWindowModel(Id)
+{
+    public override string? Title { get; }
+
+    public override int ProcessId { get; }
+
+    public override nint GetParent()
+    {
+        return 0;
+    }
+}
---
+++ src/WindowDebugger/Services/NativeWindows/NativeWindowCollectionManager.linux.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Versioning;
+using WindowDebugger.Services.NativeWindows.Windows;
+
+namespace WindowDebugger.Services.NativeWindows;
+
+partial class NativeWindowCollectionManager
+{
+    [SupportedOSPlatform("linux")]
+    private WindowsNativeWindowModel[] FindWindowsOnLinux(WindowSearchingFilter filter)
+    {
+        return [];
+    }
+}

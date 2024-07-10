31b0b4269094ebec703a6245fd9092f026eb2833
walterlv <xblero@gmail.com>
2024-07-10 12:05:24 +0800

    Tree view style.

---
+++ src/WindowDebugger/Themes/NegativeMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace WindowDebugger.Themes;
+
+public class NegativeMarginConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            Thickness thickness => new Thickness(-thickness.Left, -thickness.Top, -thickness.Right, -thickness.Bottom),
+            _ => default,
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            Thickness thickness => new Thickness(-thickness.Left, -thickness.Top, -thickness.Right, -thickness.Bottom),
+            _ => default,
+        };
+    }
+}

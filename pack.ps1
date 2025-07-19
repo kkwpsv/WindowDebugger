1b534c62a4a74c1f0dceb13b01f2aec730e62ac7
Shaojun Li <i@shaojun.li>
2025-07-19 22:50:32 +0800

    Fix pack scripts.

--- pack.ps1
+++ pack.ps1
@@ -1,10 +1,14 @@
-$currentVersion = [Version](Select-String -Path WindowDebugger/WindowDebugger.csproj "(?<=<Version>).*(?=</Version>)").Matches.Value
+$currentVersion = [Version](Select-String -Path src/WindowDebugger/WindowDebugger.csproj "(?<=<Version>).*(?=</Version>)").Matches.Value
 if (!$currentVersion) {
     Write-Output "Cannot find current version"
     exit -1
 }
 
+Write-Output "CurrentVersion: $currentVersion"
+
 $maxOldVersion = ([Version[]](git tag) | Measure-Object -Maximum).Maximum
+
+Write-Output "MaxOldVersion: $maxOldVersion"
 
 if ($maxOldVersion -and $currentVersion -le $maxOldVersion) {
     Write-Output "No need To release"
@@ -12,6 +16,9 @@ if ($maxOldVersion -and $currentVersion -le $maxOldVersion) {
 }
 
 Write-Output "Do release"
+
+dotnet publish -r win-x64 -c Release
+#TODO Linux
 
 $releaseObj =
 @{
@@ -21,11 +28,13 @@ $releaseObj =
     "assets"   = @{"links" = [System.Collections.ArrayList]@() }
 }
 
-foreach ($folderName in Get-ChildItem -Path "WindowDebugger\bin\Release" -Name) {
-    $fileName = "WindowDebugger.$currentVersion.$folderName.7z"
-    7z a $fileName .\WindowDebugger\bin\Release\$folderName\*
+foreach ($folderName in Get-ChildItem -Path "artifacts\publish\WindowDebugger" -Name) {
+    $platform = $folderName.Split('_')[1]
+    $fileName = "WindowDebugger.$currentVersion.$platform.7z"
+    Remove-Item -Path ".\artifacts\publish\WindowDebugger\$folderName\*.pdb"
+    7z a $fileName .\artifacts\publish\WindowDebugger\$folderName\*
     Invoke-WebRequest -Method Post -Uri https://upload-z2.qiniup.com -Form @{"file" = (Get-item -Path $fileName); "key" = $fileName; "token" = $env:QiniuUploadToken }
-    $releaseObj.assets.links.Add(@{"name" = [string]$folderName; "url" = "$env:QiniuReleaseUrl$fileName" })
+    $releaseObj.assets.links.Add(@{"name" = [string]$platform; "url" = "$env:QiniuReleaseUrl$fileName" })
 }
 
 Invoke-WebRequest -Method Post -Uri "$env:CI_API_V4_URL/projects/$env:CI_PROJECT_ID/releases" -ContentType "application/json" -Headers @{"JOB-TOKEN" = $env:CI_JOB_TOKEN } -Body (ConvertTo-Json -Depth 3 $releaseObj)

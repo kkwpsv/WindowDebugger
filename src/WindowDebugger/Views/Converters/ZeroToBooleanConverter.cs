4c368c850dc1dc354ebabaa2f39ce74c4257ce53
walterlv <xblero@gmail.com>
2025-12-17 16:33:58 +0800

    优化曾短暂显示过的窗口

--- src/WindowDebugger/Views/Converters/ZeroToBooleanConverter.cs
+++ src/WindowDebugger/Views/Converters/ZeroToBooleanConverter.cs
@@ -3,27 +3,6 @@ using Avalonia.Data.Converters;
 
 namespace WindowDebugger.Views.Converters;
 
-public class ZeroToStringConverter : IValueConverter
-{
-    public string? Zero { get; set; }
-
-    public string? NonZero { get; set; }
-
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value is int number)
-        {
-            return number == 0 ? Zero : NonZero;
-        }
-        return null;
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotSupportedException();
-    }
-}
-
 public class ZeroToBooleanConverter : IValueConverter
 {
     public bool Zero { get; set; }
